9d22602cdfd36af4749f490cc4d02863ad360dcc
SPUTNIK\Лекс <makdat@yandex.ru>
2016-11-15 21:27:14 +0200

    Klases darbas

---
+++ vcs.java
@@ -0,0 +1,35 @@
+package lt;
+
+public class vcs {
+
+    static float a;
+    int backup = 5;
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Antra Paskaita");
+        System.out.print("12345");
+        
+        byte baitas = 77; Byte baitas2 = new Byte(baitas);
+        baitas = 88;     
+        short trumpas = 7777; Short trumpas2 = new Short(trumpas);
+        int intas = 777777777; Integer intas2 = new Integer(777);
+        long longas = 7777777777777777777l; Long longas2 = new Long(777l);
+        
+        float floutas = 77777777777777777777777777777777777777.777777777f; Float floutas2 = new Float(floutas);
+                
+                double dabelas = 123456.34567890d; Double db1 = new Double(dabelas);
+                
+                char charas = 'f'; Character charas2 = new Character(charas);
+                
+                boolean bulenas = true; Boolean bulenas2 = new Boolean(true);
+                
+                
+                int backup = 5; // indentifikatorius backup gali prasideti doleriu zenklu ir _
+                
+                System.out.println("" + backup);
+                
+                // AntraPaskaita bla = new AntraPaskaita();
+    }
+    
+}
