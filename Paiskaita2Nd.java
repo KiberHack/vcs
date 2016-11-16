ee99c60ec86b4b98ada4d70b39e42cb461a64f41
SPUTNIK\Лекс <makdat@yandex.ru>
2016-11-16 11:35:40 +0200

    Namų darbas =)

---
+++ Paiskaita2Nd.java
@@ -0,0 +1,20 @@
+
+package lt.vcs;
+
+public class Paiskaita2Nd {
+
+    public static void main(String[] args) {
+       
+        String nd = "Padariau namų darbą ! ";
+        
+     int a = 0;
+     
+        while(a <= 100)
+        {a++;
+        System.out.println(nd);
+        }
+        
+        
+    }
+    
+}
